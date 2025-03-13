8ac4c97fbaebd461e30fc52910c915f07c2152fa
Siddhant Chauhan <chauhanjoy10@gmail.com>
2025-03-13 03:24:00 -0400

    add msg classes and constants

---
+++ Msgs/Handshake.java
@@ -0,0 +1,66 @@
+package Msgs;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class HandShakeMsg {
+    private byte[] headerBytes;
+    private byte[] peerIDBytes;
+    private byte[] zeroBits;
+    private String header;
+    private String peerID;
+
+    public HandShakeMsg() {
+        this.zeroBits = new byte[Constants.HANDSHAKE_ZEROBITS_LENGTH];
+        Arrays.fill(this.zeroBits, (byte) 0);
+    }
+
+    public HandShakeMsg(String header, String peerID) {
+        this();
+        this.header = header;
+        this.headerBytes = header.getBytes(StandardCharsets.UTF_8);
+        this.peerID = peerID;
+        this.peerIDBytes = peerID.getBytes(StandardCharsets.UTF_8);
+    }
+
+    public void setHeaderFromBytes(byte[] headerBytes) {
+        this.header = new String(headerBytes, StandardCharsets.UTF_8);
+        this.headerBytes = headerBytes;
+    }
+
+    public void setPeerIdFromBytes(byte[] peerIDBytes) {
+        this.peerID = new String(peerIDBytes, StandardCharsets.UTF_8);
+        this.peerIDBytes = peerIDBytes;
+    }
+
+    public byte[] serialize() {
+        byte[] handshakeBytes = new byte[Constants.HANDSHAKE_MESSAGE_LENGTH];
+        System.arraycopy(this.headerBytes, 0, handshakeBytes, 0, this.headerBytes.length);
+        System.arraycopy(this.zeroBits, 0, handshakeBytes, Constants.HANDSHAKE_HEADER_LENGTH, this.zeroBits.length);
+        System.arraycopy(this.peerIDBytes, 0, handshakeBytes, Constants.HANDSHAKE_HEADER_LENGTH + this.zeroBits.length, this.peerIDBytes.length);
+        return handshakeBytes;
+    }
+
+    public static HandShakeMsg deserialize(byte[] handshakeBytes) {
+        HandShakeMsg message = new HandShakeMsg();
+        byte[] header = Arrays.copyOfRange(handshakeBytes, 0, Constants.HANDSHAKE_HEADER_LENGTH);
+        byte[] peerId = Arrays.copyOfRange(handshakeBytes, Constants.HANDSHAKE_HEADER_LENGTH + Constants.HANDSHAKE_ZEROBITS_LENGTH, Constants.HANDSHAKE_MESSAGE_LENGTH);
+        message.setHeaderFromBytes(header);
+        message.setPeerIdFromBytes(peerId);
+        return message;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getPeerID() {
+        return peerID;
+    }
+
+    public void setPeerID(String peerID) {
+        this.peerID = peerID;
+        this.peerIDBytes = peerID.getBytes(StandardCharsets.UTF_8);
+    }
+}
---
+++ Msgs/Msg.java
@@ -0,0 +1,135 @@
+package Msgs;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import static Logging.Helper.logMessage;
+
+public class Msg {
+    private String type;
+    private int dataLength;
+    private byte[] payload;
+    private byte[] typeBytes;
+    private byte[] lengthBytes;
+
+    public Msg() {}
+
+    public Msg(String messageType) {
+        if (isValidMessageType(messageType)) {
+            this.dataLength = Constants.MESSAGE_TYPE;
+            this.typeBytes = messageType.trim().getBytes(StandardCharsets.UTF_8);
+            this.payload = null;
+        } else {
+            logMessage("Invalid message type.");
+        }
+    }
+
+    public Msg(String messageType, byte[] payload) {
+        this();
+        if (payload != null) {
+            this.dataLength = payload.length + 1;
+            this.payload = payload;
+        } else if (isValidMessageType(messageType)) {
+            this.dataLength = Constants.MESSAGE_TYPE;
+            this.payload = null;
+        }
+        this.type = messageType;
+        this.typeBytes = messageType.trim().getBytes(StandardCharsets.UTF_8);
+    }
+
+    private boolean isValidMessageType(String messageType) {
+        return messageType.equals(Constants.INTERESTED) || messageType.equals(Constants.NOT_INTERESTED) ||
+                messageType.equals(Constants.CHOKE) || messageType.equals(Constants.UNCHOKE) ||
+                messageType.equals(Constants.MESSAGE_DOWNLOADED);
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public byte[] getPayload() {
+        return this.payload;
+    }
+
+    public byte[] getLengthBytes() {
+        return this.lengthBytes;
+    }
+
+    public byte[] getTypeBytes() {
+        return this.typeBytes;
+    }
+
+    public void setMessageLength(int length) {
+        this.dataLength = length;
+        this.lengthBytes = ByteBuffer.allocate(Integer.BYTES).putInt(length).array();
+    }
+
+    public void setMessageLength(byte[] length) {
+        this.dataLength = ByteBuffer.wrap(length).getInt();
+        this.lengthBytes = length;
+    }
+
+    public void setMessageType(byte[] type) {
+        this.type = new String(type, StandardCharsets.UTF_8);
+        this.typeBytes = type;
+    }
+
+    public void setPayload(byte[] payload) {
+        this.payload = payload;
+    }
+
+    public static byte[] serializeMessage(Msg message) throws Exception {
+        if (!isValidMessageCode(message.getType())) {
+            throw new Exception("Invalid message type");
+        }
+
+        int totalLength = Constants.MESSAGE_LENGTH + Constants.MESSAGE_TYPE + (message.getPayload() != null ? message.getPayload().length : 0);
+        byte[] serializedMessage = new byte[totalLength];
+
+        System.arraycopy(message.getLengthBytes(), 0, serializedMessage, 0, Constants.MESSAGE_LENGTH);
+        System.arraycopy(message.getTypeBytes(), 0, serializedMessage, Constants.MESSAGE_LENGTH, Constants.MESSAGE_TYPE);
+
+        if (message.getPayload() != null) {
+            System.arraycopy(message.getPayload(), 0, serializedMessage, Constants.MESSAGE_LENGTH + Constants.MESSAGE_TYPE, message.getPayload().length);
+        }
+
+        return serializedMessage;
+    }
+
+    public static Msg deserializeMessage(byte[] message) {
+        Msg msg = new Msg();
+        byte[] msgLength = new byte[Constants.MESSAGE_LENGTH];
+        byte[] msgType = new byte[Constants.MESSAGE_TYPE];
+
+        System.arraycopy(message, 0, msgLength, 0, Constants.MESSAGE_LENGTH);
+        System.arraycopy(message, Constants.MESSAGE_LENGTH, msgType, 0, Constants.MESSAGE_TYPE);
+
+        msg.setMessageLength(msgLength);
+        msg.setMessageType(msgType);
+
+        int messageLength = ByteBuffer.wrap(msgLength).getInt();
+        if (messageLength > 1) {
+            byte[] payload = new byte[messageLength - 1];
+            System.arraycopy(message, Constants.MESSAGE_LENGTH + Constants.MESSAGE_TYPE, payload, 0, payload.length);
+            msg.setPayload(payload);
+        }
+
+        return msg;
+    }
+
+    private static boolean isValidMessageCode(String messageType) {
+        try {
+            int messageCode = Integer.parseInt(messageType);
+            return messageCode > 0 && messageCode <= 8;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public int getDataLength() {
+        return dataLength;
+    }
+
+    public void setDataLength(int dataLength) {
+        this.dataLength = dataLength;
+    }
+}
